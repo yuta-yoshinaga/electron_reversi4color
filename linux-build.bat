3328adfa830884f72e05d460cd6664464e5d0508
吉永　祐太 <yoshinaga@elecalize.co.jp>
2017-12-27 16:29:31 +0900

    ビルドバッチ調整。

--- linux-build.bat
+++ linux-build.bat
@@ -1,1 +1,1 @@
-electron-packager src Reversi --platform=linux --arch=x64 --overwrite --electron-version=1.4.15
+electron-packager src Reversi4Color --platform=linux --arch=x64 --overwrite --electron-version=1.4.15
